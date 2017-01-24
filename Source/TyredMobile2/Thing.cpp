83a6a9fad6f3319579c6265bf2ceab0ff9cefa44
Awesumson <awesumson@me.com>
2017-01-24 22:14:54 +0000

    C++ Project
    The Project is now a C++ Project

---
+++ Source/TyredMobile2/Thing.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TyredMobile2.h"
+#include "Thing.h"
+
+Thing::Thing()
+{
+}
+
+Thing::~Thing()
+{
+}
---
+++ Source/TyredMobile2/TyredMobile2.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TyredMobile2.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TyredMobile2, "TyredMobile2" );
